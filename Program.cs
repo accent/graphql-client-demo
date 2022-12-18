0b41e8b62af863a3235589c59903642e9bd66f93
jankosmala <jan.kosmala@gmail.com>
2022-12-18 12:12:59 +0100

    add queries and mutations

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using graphql_client_demo;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder(args);
+builder.ConfigureServices(services =>
+{
+    services.AddClient().ConfigureHttpClient(client => { client.BaseAddress = new Uri("https://localhost:7038/graphql"); });
+});
+var buildServices = builder.Build();
+
+var graphqlClient = buildServices.Services.GetRequiredService<Client>();
+var allBooks = await graphqlClient.GetAllBooks.ExecuteAsync();
+
+foreach (var book in allBooks.Data.Book)
+{
+    Console.WriteLine($"[{book.BookId}] {book.Title}, ISBN: {book.Isbn}");
+}
+
+var authorResponse = await graphqlClient.GetAuthor.ExecuteAsync(1);
+var author = authorResponse.Data.Author;
+Console.WriteLine($"Author: {author.FirstName} {author.LastName}");
+
+var authorRemovalResult = await graphqlClient.RemoveAuthor.ExecuteAsync("4");
+Console.WriteLine($"Author removed? {authorRemovalResult.Data.Boolean}");
